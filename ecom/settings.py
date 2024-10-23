5e430b2f8941019852696fa4995fedcc35b2944d
suyog ojha <ojhasuyog2020@gmail.com>
2024-10-23 14:57:51 +0100

    @

--- ecom/settings.py
+++ ecom/settings.py
@@ -2,10 +2,13 @@
 
 from pathlib import Path
 import os 
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DATABASE_URL = "postgresql://postgres:BgizzUjUUJzjKuJfIfwPpMcZkejkECbT@junction.proxy.rlwy.net:46414/railway"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -70,10 +73,7 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
 
 
