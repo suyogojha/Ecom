5e00b1f5afd7535772b3754f3f01fd290fb9daa5
suyog ojha <ojhasuyog2020@gmail.com>
2024-11-23 01:20:29 +0000

    V.1.3

--- ecom/settings.py
+++ ecom/settings.py
@@ -121,11 +121,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = ['static/'] 
 STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = ['static/'] 
+# STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
+
+# MEDIA_URL = 'media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
