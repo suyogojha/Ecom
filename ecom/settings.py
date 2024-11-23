50c2010552b146f710a32e08f1cd74c060820983
Suyog Ojha <65763177+suyogojha@users.noreply.github.com>
2024-11-23 01:24:33 +0000

    Update settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -126,12 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 STATIC_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-# STATIC_URL = 'static/'
-# STATICFILES_DIRS = ['static/'] 
-# STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 
-# MEDIA_URL = 'media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
