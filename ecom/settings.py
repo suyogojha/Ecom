fdd5c160df025a5b6c5592d1e2989d8e95de3050
suyog ojha <ojhasuyog2020@gmail.com>
2024-11-23 01:27:26 +0000

    V.1.3

--- ecom/settings.py
+++ ecom/settings.py
@@ -120,10 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_DIRS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 # STATIC_URL = 'static/'
